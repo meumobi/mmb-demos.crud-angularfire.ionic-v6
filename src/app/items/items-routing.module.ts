3700b89bababe24542fc437df5092a230a46fa5d
vdias38 <victor.dias@meumobi.com>
2022-03-28 12:21:10 +0100

    add master-detail pages and routes

---
+++ src/app/items/items-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ItemDetailComponent } from './item-detail/item-detail.component';
+import { ItemListComponent } from './item-list/item-list.component';
+
+const routes: Routes = [
+  { path: '', component: ItemListComponent },
+  { path: 'detail/:id', component: ItemDetailComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class ItemsRoutingModule {}
